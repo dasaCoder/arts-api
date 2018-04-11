58dfe00591c7fa3f31c83468eda11520037f3277
Dilusha <dilushadasanayaka@gmail.com>
2018-04-11 10:55:08 +0530

    splash screen added

---
+++ app/src/main/java/com/example/acer/artsapi/Splash_screen.java
@@ -0,0 +1,34 @@
+package com.example.acer.artsapi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_res);
+
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                    startActivity(intent);
+
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        splashThread.start();
+    }
+}
